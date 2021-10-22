86a3cf6475f3339847c74ab5cb36e3bb4d0a582f
Kirill Ki <kirillkalapov@gmail.com>
2021-10-22 15:08:51 +0200

    create smooth line corners

--- YourWorkoutTimer/TimerViewController.swift
+++ YourWorkoutTimer/TimerViewController.swift
@@ -9,19 +9,22 @@ import UIKit
 
 class TimerViewController: UIViewController {
 
+    let shapeLayer = CAShapeLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let center = view.center
         
-        let shapeLayer = CAShapeLayer()
+        
         let circularPath = UIBezierPath(arcCenter: center, radius: 100,
-                                        startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+                                        startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
         shapeLayer.path = circularPath.cgPath
         shapeLayer.strokeColor = #colorLiteral(red: 0.6520697474, green: 0.7128807902, blue: 0.7406589985, alpha: 0.8470588235)
         shapeLayer.lineWidth = 10
         shapeLayer.strokeEnd = 0
         shapeLayer.fillColor = #colorLiteral(red: 0.1882352941, green: 0.2666666667, blue: 0.3058823529, alpha: 1)
+        shapeLayer.lineCap = .round
         view.layer.addSublayer(shapeLayer)
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
 
@@ -33,10 +36,11 @@ class TimerViewController: UIViewController {
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
         basicAnimation.toValue = 1
         basicAnimation.duration = 2
-       // basicAnimation.fillMode = kCAFillMode
+        basicAnimation.fillMode = CAMediaTimingFillMode.forwards
+        basicAnimation.isRemovedOnCompletion = false
+       
         
-        
-        shapeLayer.add(CAAnimation)
-    }
+        shapeLayer.add(basicAnimation, forKey: "urSoBasic")
   
+}
 }
