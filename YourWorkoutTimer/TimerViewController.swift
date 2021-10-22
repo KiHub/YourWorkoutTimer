d0b126583e8da0f68a11d8b5b80abbcb1f2e2ef4
Kirill Ki <kirillkalapov@gmail.com>
2021-10-22 15:17:19 +0200

    Second screen done

--- YourWorkoutTimer/TimerViewController.swift
+++ YourWorkoutTimer/TimerViewController.swift
@@ -16,14 +16,27 @@ class TimerViewController: UIViewController {
         
         let center = view.center
         
+        // create track layer
+        let trackLayer = CAShapeLayer()
         
         let circularPath = UIBezierPath(arcCenter: center, radius: 100,
                                         startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        trackLayer.path = circularPath.cgPath
+        trackLayer.strokeColor = #colorLiteral(red: 0.2437945306, green: 0.3365195096, blue: 0.3791738749, alpha: 0.8470588235)
+        trackLayer.lineWidth = 10
+       // trackLayer.strokeEnd = 0
+        trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.lineCap = .round
+        view.layer.addSublayer(trackLayer)
+        
+        
+//        let circularPath = UIBezierPath(arcCenter: center, radius: 100,
+//                                        startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
         shapeLayer.path = circularPath.cgPath
         shapeLayer.strokeColor = #colorLiteral(red: 0.6520697474, green: 0.7128807902, blue: 0.7406589985, alpha: 0.8470588235)
         shapeLayer.lineWidth = 10
         shapeLayer.strokeEnd = 0
-        shapeLayer.fillColor = #colorLiteral(red: 0.1882352941, green: 0.2666666667, blue: 0.3058823529, alpha: 1)
+        shapeLayer.fillColor = UIColor.clear.cgColor
         shapeLayer.lineCap = .round
         view.layer.addSublayer(shapeLayer)
         view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
